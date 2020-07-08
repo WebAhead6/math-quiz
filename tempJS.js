ca0dee0fd44c83f2a0b1f60e42caf932cf0b6bd8
Bader Fahoum <fahoumbader@gmail.com>
2020-07-08 13:37:02 +0300

    this is the new logic of the game

--- tempJS.js
+++ tempJS.js
@@ -1,51 +1,109 @@
+// ------------------------------------------------ defintions ------------------------------------------------------------------
 // array that holds all the correct answers
-const correctAnswer = ["A", "B", "C", "D"];
+// const correctAnswer = ["A", "B", "C", "D"];
+const nextBtn = document.querySelector("#nextBTN");
+const answer = document.querySelector("#answer");
+const startBtn = document.querySelector("#startBtn");
+let answerfield = document.querySelector('.asnwer-field');
+let timer = document.querySelector('.timer');
+let currentQuestion = 0;
+let scoreCounter = 0;
+let messageText = [
+  "you are really braindead",
+  "you will be accepted in society",
+  "nice",
+];
+
+let questions = [
+  { question: "5*2", answer: "10" },
+  { question: "3+7*2", answer: "17" },
+  { question: "3-5*2", answer: "-7" },
+  { question: "6/2(1+2)", answer: "9" },
+];
+
 const form = document.querySelector(".quiz-form");
 const allqs = document.querySelectorAll("div.my-5");
 const result = document.querySelector(".result");
-const submitBtn = document.querySelector("#submitbtn");
+// const questions = document.querySelector('#questions');
 // allqs.forEach( (elemenet,index) => {
 //     console.log(elemenet,index);
 // });
 // console.log(form);
-
+//---------------------------------------------------------- Submission LOGIC! -------------------------------------------------------
 const submit = (e) => {
   console.log("EVENT INVOKED");
-  if (e) e.preventDefault();
-  console.log("788fyeugwdjkOEWGPREOIUYBEFHSJAKDNLM");
-  let score = 0;
-  console.log(form.q1);
-  let answers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-  console.log(answers);
-  // check answers
-  answers.forEach((answer, index) => {
-    if (answer === correctAnswer[index]) {
-      score += 25;
-    }
-  });
+  if (e) {
+    e.preventDefault();
+  }
+  //   let score = 0;
+  //   console.log(form.q1);
+  //   let answers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  //   console.log(answers);
+  //   // check answers
+  //   answers.forEach((answer, index) => {
+  //         if (answer === correctAnswer[index]) {
+  //         score += 25;
+  //         }
+  //     })
   // console.log(score);
   //show result on page
   // we dont need to add the window object to use scrollTo, but this was done to emphasis the use of the global object
+
   window.scrollTo(0, 0);
   result.classList.remove("d-none");
-
   let output = 0;
   const timer = setInterval(() => {
-    result.querySelector("span").textContent = `${output}%`;
-    if (output === score) {
+    result.querySelector("span").textContent = `${output}`;
+    if (output === scoreCounter) {
       clearInterval(timer);
     } else {
       output++;
     }
   }, 10);
+
+  hideTimer();
 };
 
 form.onsubmit = submit;
 
-// timer SECTION \1\111111!!!!!!!!!!!!
+//-------- next question logic -------
+answerfield.classList.add('d-none');
+nextBtn.classList.add('d-none');
+
+nextBtn.addEventListener("click", function () {
+    console.log(answer.value);
+  if (answer.value == questions[currentQuestion].answer) {
+    // alert('Correct Answer!');
+    scoreCounter+=5;
+  }
+  answer.value = "";
+  currentQuestion++;
+  if (currentQuestion == questions.length) {
+    //   question.style.display = "none";
+    submit();
+  }
+
+  questionText.textContent = questions[currentQuestion].question;
+});
+
+  startBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    startBtn.classList.add('d-none');
+    nextBtn.classList.remove('d-none');
+    answerfield.classList.remove('d-none');
+
+    questionText.textContent = questions[currentQuestion].question;
+    var interval = setInterval(setTime, 1000);
+    timer.classList.remove('d-none');
+
+  });
+
+// --------------------------------------------------------------- SUBMISSION LOGIC END! ------------------------------------------
+
+// --------------------------------------------------------------- TIMER SECTION --------------------------------------------------
 var minutesLabel = document.getElementById("minutes");
 var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 2;
+var totalSeconds = 600;
 
 function setTime() {
   totalSeconds--;
@@ -57,18 +115,15 @@ function setTime() {
     const inputs = document.querySelectorAll("input");
     clearInterval(interval);
     inputs.forEach((e) => {
-      if (e.classList.contains("btn")) {
-        e.disabled = false;
+      if (e.classList.contains("btn-light")) {
+      } else {
+        e.disabled = true;
       }
-      e.disabled = true;
     });
-    minutesLabel.classList.add("d-none");
-    secondsLabel.classList.add("d-none");
-    minutesLabel.nextElementSibling.classList.add("d-none");
     submit();
+    hideTimer();
   }
 }
-var interval = setInterval(setTime, 1000);
 
 function pad(val) {
   var valString = val + "";
@@ -78,3 +133,12 @@ function pad(val) {
     return valString;
   }
 }
+
+function hideTimer() {
+  minutesLabel.classList.add("d-none");
+  secondsLabel.classList.add("d-none");
+  minutesLabel.nextElementSibling.classList.add("d-none");
+}
+//-------------------------------------------------------- TIMER SECTION END -----------------------------------------------
+
+//-------------------------------------------------------- QUESTION LOGIC ---------------------------------------------------
